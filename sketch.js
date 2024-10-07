afed2f338d92f3234a9919e8f6421021931ec889
andrewxzin1 <estacio.andrew@escola.pr.gov.br>
2024-10-07 11:40:10 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// depois da chuva, 10, drama
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, 
+  function draw() {
+    background (220);
+    let recomendacao = "A viagem de chihiro";
+}function draw() {
+    background (220);
+    let recomendacao = "A viagem de chihiro";
+    text(recomendacao, width/2, height/2)
+  function draw() {
+    background (220);
+    let recomendacao = geraRecomendacao();
+    text(recomendacao, width/2, height/2);
+ function geraRecomendacao() {
+  return "A viagem de Chihiro";
+}function geraRecomendacao() {
+    if (idade >= 10) {
+        return "As aventuras de Pi";
+    } else {
+        return "A viagem de Chihiro";
+    }
+}function geraRecomendacao(idade) {
+    if(idade >= 10) {
+        return "As aventuras de Pi";
+    } else {
+        return "A viagem de Chihiro"; 
+      
